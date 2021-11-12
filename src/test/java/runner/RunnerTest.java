cab8e6e0dae8ef119c5aa2d69f88195dceaeca03
Lucas <lucasalencarlino@hotmail.com>
2021-11-12 12:31:33 -0200

    modificacao de pasta no runner

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
       features = "src/test/resources/features/",
-      tags = "@RealizaCompraProduto",
-      glue = "br.com.chronosacademy.steps",
+      tags = "@SuiteLojaAutomation",
+      glue = "steps",
       plugin = {"json:target/reports/CucumberReports.json", "pretty"},
       snippets = CucumberOptions.SnippetType.CAMELCASE
 
